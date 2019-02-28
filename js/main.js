02d97de11070da48e4a245ef732be785dc3c7e7f
Romantech3000 <romantech3000@gmail.com>
2019-02-28 18:43:18 +0300

    extra product card elements and styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const IMG_DIR = 'assets/images/';
+const currencyTag = '&euro;';
+
 const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 20},
@@ -5,12 +8,17 @@ const products = [
     {id: 4, title: 'Gamepad', price: 50},
 ];
 
-const renderProduct = (title = 'No Title', price = 999999.99, image = 'assets/images/no_image.jpg') => {
+const renderProduct = (title = 'No Title', price = 999999.99, image = 'no_image.jpg') => {
+    //Shouldn't I be creating it using DOM? Works for practicing ES6 template strings though
     return `<div class="product-item">
+                <a class="product-item-link"  href="#">
                 <h3 class="product-item-title">${title}</h3>
-                <img src="${image}">
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+                <img src="${IMG_DIR + image}" alt="${title}">
+                <div class="product-item-bottom-flex">
+                    <p class="product-item-price">${price + " " + currencyTag}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+                </a>
             </div>`;
 };
 
