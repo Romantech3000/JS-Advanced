8b634486eddc9f11971f1cf2115afdf7e55a0794
Romantech3000 <romantech3000@gmail.com>
2019-03-09 03:36:39 +0300

    l8 upd8

--- Task2and3/js/main.js
+++ Task2and3/js/main.js
@@ -1,8 +1,9 @@
 const API = 'https://raw.githubusercontent.com/Romantech3000/assets/master/homework-a/json';
 const IMG_DIR = 'https://raw.githubusercontent.com/Romantech3000/assets/master/homework-a/images/';
+const TMB_DIR = 'https://raw.githubusercontent.com/Romantech3000/assets/master/homework-a/thumbnails/';
 
 //const IMG_DIR = 'assets/images/';
-const TMB_DIR = 'assets/thumbnails/'; //thumbnails
+//const TMB_DIR = 'assets/thumbnails/';
 let currencyTag = '&euro;';
 
 
@@ -43,6 +44,10 @@ class ProductsList {
         block.insertAdjacentHTML('afterend', `<p class="cart-total">Стоимость товаров: ${this.getTotal()} ${currencyTag}</p>`);
     }
 
+    getProductById(prodId) {
+        return this.allProducts.find((el) => el.id === prodId);
+    }
+
     getTotal() {
         return this.allProducts.reduce( (total, prod) => total + prod.price, 0 );
     }
@@ -77,6 +82,7 @@ class ShopCart {
         this.container = container;
         this.products = [];
         this.isVisible = false;
+        this.render();
     }
 
     get visible() {
@@ -101,26 +107,32 @@ class ShopCart {
     addProduct(product, quantity = 1) {
         const prodIdx = this.products.findIndex(prod => prod.id === product.id);
         if (prodIdx < 0) {
+
             this.products.push(product);
-            this.products[this.products.products.length-1].quantity = quantity;
+            this.products[this.products.length-1].quantity = quantity;
         }
         else {
             this.products[prodIdx].quantity += quantity;
         }
     }
 
-    delProduct(prodId) {
+    delProduct(prodId, delAll = true) {
         const prodIdx = this.products.findIndex(prod => prod.id === prodId);
-        if (prodIdx >= 0) this.products.splice(prodIdx, 1);
+        if (prodIdx >= 0) {
+            this.products[prodIdx].quantity--;
+            if (delAll || this.products[prodIdx].quantity < 1) this.products.splice(prodIdx, 1);
+        }
     }
 
     render() {
         const containerEl = document.querySelector(this.container);
         let content = '';
-        this.products.forEach(prod => {
+        this.products.forEach(product => {
             const cartItem = new CartItem(product);
             content += cartItem.render();
         });
+        if (this.products.length) content += `<p>total: ${this.getTotal()}</p>`;
+        else content +='<p>Корзина пуста</p>';
         containerEl.innerHTML = content;
     }
 
@@ -134,11 +146,12 @@ class ShopCart {
 }
 
 class CartItem {
-    constructor(product) {
-        this.id = prodict.id;
+    constructor(product, img = 'no_image.jpg') {
+        this.id = product.id;
         this.price = product.price;
         this.title = product.title;
         this.quantity = product.quantity;
+        this.img = img;
     }
 
     render() {
@@ -148,15 +161,14 @@ class CartItem {
                     <p class="cart-item-title">${this.title}</p>
                 </div>
                 <div class = "cart-item-w1">
-                    <p class="product-item-price">${this.price + " " + currencyTag}</p>
+                    <p class="cart-item-price">${this.price + " " + currencyTag}</p>
                 </div>
                 <div class = "cart-item-w1">
                     ${this.quantity}
                 </div>
-                <div>
-                    <button class="del-btn" data-id="${this.id}">Удалить</button>
+                <div class = "cart-item-w1">
+                    <button class="del-btn" data-id="${this.id}" title="Удалить"></button>
                 </div>
-            </a>
         </div>`;
     }
 }
@@ -172,15 +184,25 @@ function init() {
         showCartEl.addEventListener('click', () => { cart.visible = !cart.visible});
     }
 
+    // should consider moving it inside the class
     const prodsEl = document.querySelector('.products');
-
     prodsEl.addEventListener('click', (e) => {
         if (e.target.classList.contains('buy-btn')) {
-            let prodId = +e.target.dataset.id;
-            console.log(prodId);
+            cart.addProduct(list.getProductById(+e.target.dataset.id));
+            cart.render();
             e.preventDefault();
-        };
-    })
+        }
+    });
+
+    const cartEl = document.querySelector('.cart');
+    cartEl.addEventListener('click', (e) => {
+        if (e.target.classList.contains('del-btn')) {
+            console.log(e.target.dataset.id);
+            cart.delProduct(+e.target.dataset.id, false);
+            cart.render();
+            e.preventDefault();
+        }
+    });
 }
 
 
